dd841d6bf133c0cd1b1e77c6ba925a2dc13140d8
Durga Rachamallu <rachamallu.durga@gmail.com>
2026-03-08 18:28:43 -0400

    Updated file1 with upper cased name

--- file1.py
+++ file1.py
@@ -5,8 +5,9 @@
 
 
 def print_hi(name):
+    upper_name = upper(name)
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {upper_name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
